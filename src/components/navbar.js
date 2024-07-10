bd93281d083dcf5763b8c23ad9de606e0d7dc971
romankochkov <roman.kochkov@hotmail.com>
2024-07-09 23:52:55 -0400

    Added mobile adaptation for the navigation bar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -30,19 +30,21 @@ export default function Navbar() {
         <nav className={`${styles.nav} ${hideNav ? styles.hide : ''}`}>
             <div className="container">
                 <div className="row">
-                    <div className="col-12">
-                        <div className="d-flex justify-content-between">
-                            <div className="d-flex justify-content-start" style={{ minWidth: '90px' }}>
-                                <div className={styles.logo}>
-                                    <Image
-                                        src="/logo.svg"
-                                        width={32}
-                                        height={32}
-                                        quality={100}
-                                        alt="LOGO"
-                                    />
-                                </div>
+                    <div className="col-6 col-sm-6 col-md-6 col-lg-2">
+                        <div className="d-flex justify-content-start" style={{ height: '100%' }}>
+                            <div className={styles.logo}>
+                                <Image
+                                    src="/logo.svg"
+                                    width={32}
+                                    height={32}
+                                    quality={100}
+                                    alt="LOGO"
+                                />
                             </div>
+                        </div>
+                    </div>
+                    <div className="d-none d-lg-block col-lg-8">
+                        <div className="d-flex justify-content-center" style={{ height: '100%' }}>
                             <div className={styles.links}>
                                 <div className={styles.home}><a href="/">Home</a></div>
                                 <div className={styles.tshirts}><a href="/t-shirts">T-Shirts</a></div>
@@ -50,17 +52,20 @@ export default function Navbar() {
                                 <div className={styles.tracking}><a href="/tracking">Tracking</a></div>
                                 <div className={styles.news}><a href="/news">News</a></div>
                             </div>
-                            <div className="d-flex justify-content-end" style={{ minWidth: '90px' }}>
-                                <Image
-                                    src="/usa.svg"
-                                    className={styles.language}
-                                    width={28.5}
-                                    height={18}
-                                    quality={100}
-                                    alt="EN"
-                                />
-                                <div className={styles.cart}><i className="fa-regular fa-cart-shopping"></i></div>
-                            </div>
+                        </div>
+                    </div>
+                    <div className="col-6 col-sm-6 col-md-6 col-lg-2">
+                        <div className="d-flex justify-content-end" style={{ height: '100%' }}>
+                            <Image
+                                src="/usa.svg"
+                                className={`${styles.language} d-none d-md-block`}
+                                width={28.5}
+                                height={18}
+                                quality={100}
+                                alt="EN"
+                            />
+                            <div className={styles.cart}><i className="fa-regular fa-cart-shopping"></i></div>
+                            <div className={`${styles.sidebar} d-block d-lg-none`}><i className="fa-solid fa-bars"></i></div>
                         </div>
                     </div>
                 </div>
